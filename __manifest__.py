3d5d710c0182096ba37e13d36ff0cc9707eea421
hichem-zahaf <hich3m.zahaf@gmail.com>
2026-02-04 07:59:19 +0100

    removed sidebar and changed li

--- __manifest__.py
+++ __manifest__.py
@@ -19,23 +19,13 @@
             'jameel_backend_theme/static/src/scss/login.scss',
         ],
         'web.assets_backend': [
-            # SCSS - Load in order (variables first, then components, then main style)
+            # SCSS - Load in order (variables first, then main style)
             'jameel_backend_theme/static/src/scss/variables.scss',
-            'jameel_backend_theme/static/src/scss/components/_animations.scss',
-            'jameel_backend_theme/static/src/scss/components/_buttons.scss',
-            'jameel_backend_theme/static/src/scss/components/_tables.scss',
-            'jameel_backend_theme/static/src/scss/components/_forms.scss',
-            'jameel_backend_theme/static/src/scss/components/_cards.scss',
-            'jameel_backend_theme/static/src/scss/components/_dialogs.scss',
-            'jameel_backend_theme/static/src/scss/components/_navbar.scss',
-            'jameel_backend_theme/static/src/scss/components/_responsive.scss',
+            'jameel_backend_theme/static/src/scss/navigation_bar.scss',
+            'jameel_backend_theme/static/src/scss/override.scss',
             'jameel_backend_theme/static/src/scss/style.scss',
             # XML Templates
             'jameel_backend_theme/static/src/xml/styles.xml',
-            'jameel_backend_theme/static/src/xml/sidebar.xml',
-            'jameel_backend_theme/static/src/xml/top_bar.xml',
-            # JavaScript (only NavBarJameel.js for sidebar functionality)
-            'jameel_backend_theme/static/src/js/NavBarJameel.js',
         ],
     },
     'images': [
@@ -43,7 +33,7 @@
         'static/description/theme_screenshot.jpg',
     ],
     'icon': 'static/description/icon.png',
-    'license': 'MIT',
+    'license': 'LGPL-3',
     'pre_init_hook': 'init_hooks',
     'post_init_hook': 'init_hooks',
     'installable': True,
